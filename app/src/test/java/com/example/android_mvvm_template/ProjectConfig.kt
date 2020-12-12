b2fde1d05f68486f1aa214dc11f67c69d1f7c3b0
이용준 <wooooooak@gmail.com>
2020-12-12 21:30:50 +0900

    Add Kotest project config

---
+++ app/src/test/java/com/example/android_mvvm_template/ProjectConfig.kt
@@ -0,0 +1,8 @@
+package com.example.android_mvvm_template
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.spec.IsolationMode
+
+object ProjectConfig : AbstractProjectConfig() {
+    override val isolationMode: IsolationMode = IsolationMode.InstancePerTest
+}
