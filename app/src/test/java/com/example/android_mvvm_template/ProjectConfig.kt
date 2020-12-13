74945758c79e221cf0027bd309518ea7b92e4228
이용준 <wooooooak@gmail.com>
2020-12-13 14:31:43 +0900

    Project Config setting 변경
    InstancePerTest -> InstancePerLeaf

--- app/src/test/java/com/example/android_mvvm_template/ProjectConfig.kt
+++ app/src/test/java/com/example/android_mvvm_template/ProjectConfig.kt
@@ -4,5 +4,5 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
 
 object ProjectConfig : AbstractProjectConfig() {
-    override val isolationMode: IsolationMode = IsolationMode.InstancePerTest
+    override val isolationMode: IsolationMode = IsolationMode.InstancePerLeaf
 }
